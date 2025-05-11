d6c979f67bc575e06239583db57f4d150cd6ce07
heewonn09 <heewonn09@naver.com>
2025-05-12 00:42:58 +0900

    Fix message

--- src/Image.jsx
+++ src/Image.jsx
@@ -5,7 +5,7 @@ import { Image as RNImage } from 'react-native';
 const StyledImage = styled(RNImage)`
   width: ${({ width }) => width || '100px'};
   height: ${({ height }) => height || '100px'};
-  margin-bottom: ${({ marginBottom }) => marginBottom || '20px'};
+   margin-bottom: ${({ marginBottom }) => marginBottom || '20px'};
 `;
 
 
